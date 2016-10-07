4d7faf5bc8e05d2458965b0015be0a3a83872e8a
alehuo <aleksi.huotala@helsinki.fi>
2016-10-07 19:34:43 +0300

    Fix Heroku PostgreSQL Error
    Hopefully fixed for real this time

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -124,13 +124,13 @@ public class Database {
         lista.add("CREATE TABLE categories (categoryId SERIAL PRIMARY KEY, title varchar(255));");
         //Alakategoriat -taulu
         lista.add("CREATE TABLE subCategories (subCatId SERIAL PRIMARY KEY, catId integer , title varchar(255), description varchar(1024), FOREIGN KEY(catId) REFERENCES categories(categoryId));");
+        //Käyttäjät
+        lista.add("CREATE TABLE users (userId SERIAL PRIMARY KEY, username varchar(255), password varchar(255), salt varchar(255), userLevel integer);");
         //Viestiketjut
         lista.add("CREATE TABLE threads (threadId SERIAL PRIMARY KEY, subCategoryId integer , userId integer, title varchar(255), creationDate varchar(255), FOREIGN KEY(subCategoryId) REFERENCES subCategories(subCatId), FOREIGN KEY(userId) REFERENCES users(userId));");
         //Viestiketjun postaukset
         lista.add("CREATE TABLE posts (postId SERIAL PRIMARY KEY, threadId integer , userId integer , timestamp varchar(255), body varchar(4096), FOREIGN KEY(threadId) REFERENCES threads(threadId), FOREIGN KEY(userId) REFERENCES users(userId));");
-        //Käyttäjät
-        lista.add("CREATE TABLE users (userId SERIAL PRIMARY KEY, username varchar(255), password varchar(255), salt varchar(255), userLevel integer);");
-
+        
         //Kategoriat
 //        lista.add("INSERT INTO categories (title) VALUES ('Testikategoria 1');");
 //        lista.add("INSERT INTO categories (title) VALUES ('Testikategoria 2');");
