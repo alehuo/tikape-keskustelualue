12bb2c9330dc93fef2d112cdeb0932e6210934df
Alehuo <aleksi.huotala@helsinki.fi>
2016-10-28 22:29:47 +0300

    Cleanup

--- src/main/java/tikape/runko/database/CategoryDao.java
+++ src/main/java/tikape/runko/database/CategoryDao.java
@@ -20,10 +20,11 @@ public class CategoryDao implements Dao<Category, Integer> {
      * Yläkategorian DAO
      *
      * @param db Database
+     * @param subCategoryDao Alakategorian DAO
      */
-    public CategoryDao(Database db) {
+    public CategoryDao(Database db, SubCategoryDao subCategoryDao) {
         this.database = db;
-        subCategoryDao = new SubCategoryDao(db);
+        this.subCategoryDao = subCategoryDao;
     }
 
     /**
@@ -35,19 +36,25 @@ public class CategoryDao implements Dao<Category, Integer> {
      */
     @Override
     public Category findOne(Integer key) throws SQLException {
+
         Connection connection = database.getConnection();
+
         PreparedStatement stmt = connection.prepareStatement("SELECT * FROM categories WHERE categoryId = ?");
         stmt.setObject(1, key);
 
         ResultSet rs = stmt.executeQuery();
+
         boolean hasOne = rs.next();
+
         if (!hasOne) {
             return null;
         }
 
         Integer id = rs.getInt("categoryId");
         String title = rs.getString("title");
+
         Category cat = new Category(id, title);
+
         //Lisätään myös alakategoriat Category-olioon.
         cat.setSubCategories(subCategoryDao.findAllByCategoryId(id));
 
@@ -66,17 +73,23 @@ public class CategoryDao implements Dao<Category, Integer> {
      */
     @Override
     public List<Category> findAll() throws SQLException {
+
         Connection connection = database.getConnection();
+
         PreparedStatement stmt = connection.prepareStatement("SELECT * FROM categories");
         ResultSet rs = stmt.executeQuery();
 
         List<Category> categories = new ArrayList<>();
         while (rs.next()) {
+
             Integer id = rs.getInt("categoryId");
             String title = rs.getString("title");
-            //Lisätään myös alakategoriat Category-olioon.
-            categories.add(new Category(id, title).setSubCategories(subCategoryDao.findAllByCategoryId(id)));
 
+            //Lisätään myös alakategoriat Category-olioon.
+            Category c = new Category(id, title);
+            c.setSubCategories(subCategoryDao.findAllByCategoryId(id));
+
+            categories.add(c);
         }
 
         rs.close();
@@ -94,9 +107,12 @@ public class CategoryDao implements Dao<Category, Integer> {
      */
     @Override
     public void delete(Integer key) throws SQLException {
+
         Connection connection = database.getConnection();
+
         PreparedStatement stmt = connection.prepareStatement("DELETE FROM categories WHERE categoryId = ?");
         stmt.setInt(1, key);
+
         stmt.execute();
         stmt.close();
     }
@@ -109,8 +125,10 @@ public class CategoryDao implements Dao<Category, Integer> {
      */
     public void add(Category c) throws SQLException {
         Connection connection = database.getConnection();
+        
         PreparedStatement stmt = connection.prepareStatement("INSERT INTO categories (title) VALUES (?)");
         stmt.setString(1, c.getName());
+        
         stmt.execute();
         stmt.close();
     }
